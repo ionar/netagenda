1048a30b2dd29d14d591785ddbcf574aa44746ef
Ionar Rafael <irm@ionar.com.br>
2019-01-30 17:55:26 -0200

    create page cadastros page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def index
   end
   def success
+  end
+  def cadastros
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
   resources :calendars
   resources :services
   get 'welcome/index'
-  get 'welcome/success'
+  get 'welcome/cadastros'
   resources :clients
-  root to: 'welcome#index'
+  root to: 'appointments#index'
   devise_for :users, :path_prefix => 'my', controllers: {
         sessions: 'users/sessions'
   }
   resources :users
 
   get "/qual_dia" => 'appointments#diasel', as: 'qual_dia'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   ##get "/myCalendar" => 'appointments#calendar'
