4291c444a0851d4b8c37c5ed17609139aa5f3004
Ionar Rafael <irm@ionar.com.br>
2019-03-19 16:44:31 -0300

    disable calendar in clients form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,14 +4,14 @@
 //= require jquery3
 //= require serviceworker-companion
 //= require materialize
-//= require moment 
+//= require moment
 //= require fullcalendar
 //= require fullcalendar/locale-all
 //= require_tree .
 
 
   function eventCalendar() {
-    return $('#calendar').fullCalendar({ 
+    return $('#calendar').fullCalendar({
       dayClick: function(date, allDay, jsEvent, view) {
       dia = moment.utc(date).format('DD-MM-YYYY');
 
@@ -50,7 +50,7 @@
   };
 
   function clearCalendar() {
-    $('#calendar').fullCalendar('delete'); 
+    $('#calendar').fullCalendar('delete');
     $('#calendar').html('');
   };
 
@@ -70,22 +70,22 @@ $(document).on("turbolinks:load", function() {
   //Tentando customizar o fullcalendar
   //$('.fc-center h2').addClass("flow-text");
 
-  M.Modal._count = 0; 
+  M.Modal._count = 0;
   $('.modal').modal();
   //M.textareaAutoResize($('.materialize-textarea')); //colocar em cada form
   M.updateTextFields();
   $('select').formSelect();
-  $('.dropdown-button').dropdown(); 
+  $('.dropdown-button').dropdown();
   $('input#input_text, textarea#textarea2').characterCounter();
   $('.dropdown-trigger').dropdown();
-   Waves.displayEffect(); 
+   Waves.displayEffect();
    window.materializeForm.init();
   $('.sidenav').sidenav();
 
   $('.abre-calendario-hora').timepicker({
     twelveHour: false,
   });
-  
+
 
   $('.abre-calendario').datepicker({
   	selectMonths: true,
@@ -116,9 +116,9 @@ $(document).on("turbolinks:load", function() {
 	    weekdays: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'],
 	    monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
 	    months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-	}  
+	}
   });
- 
+
 });
 
 // Fix bug with turbolinks and sidenav
