8fbea59997a47f63a5452dfb08cf058d8f0b3bc7
Ionar Rafael <irm@ionar.com.br>
2019-12-06 08:05:03 -0300

    fix syntax for turbolinks auto reload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -135,13 +135,13 @@ $(document).on('ready page:load', function() {
   var REFRESH_INTERVAL_IN_MILLIS = 5000;
    if ($('.f-pending-message').length > 0) {
      setTimeout(function(){
-      #disable page scrolling to top after loading page content
+      // disable page scrolling to top after loading page content
       Turbolinks.enableTransitionCache(true);
 
-      # pass current page url to visit method
+      // pass current page url to visit method
       Turbolinks.visit(location.toString());
 
-      #enable page scroll reset in case user clicks other link
+      // enable page scroll reset in case user clicks other link
       Turbolinks.enableTransitionCache(false);
        }, REFRESH_INTERVAL_IN_MILLIS);
   }
