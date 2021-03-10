db95848ccad3647b8317f7891c8d3bf711f78df7
Ionar <irm@ionar.com.br>
2021-03-10 11:26:23 -0300

    test with 300 last results

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -60,7 +60,8 @@ class AppointmentsController < ApplicationController
     @appointments_mes = Appointment.where(schedule_on: inicio_do_mes..fim_do_mes)
     @appointments_mes = @appointments_mes.para_o_calendar(current_user.calendar_id)
 
-    @appointments_all = Appointment.where(nil)
+    ##@appointments_all = Appointment.where(nil)
+    @appointments_all = Appointment.last(300)
     @appointments_all = @appointments_all.para_o_calendar(current_user.calendar_id)
 
   end
